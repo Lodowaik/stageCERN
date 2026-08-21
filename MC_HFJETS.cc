fad217f18efe87fa71c8280062010ddf31ff221b
Ludovica Marsella <LA_MIA_EMAIL_GITHUB>
2026-08-21 10:05:51 +0200

    Modifiche del 15 agosto

--- MC_HFJETS.cc
+++ MC_HFJETS.cc
@@ -19,11 +19,11 @@
 namespace Rivet {
 
 
-  class MC_HFJETS : public Analysis {
+  class MC_HFJETS_MOD : public Analysis {
   public:
 
     // Constructor
-    RIVET_DEFAULT_ANALYSIS_CTOR(MC_HFJETS);
+    RIVET_DEFAULT_ANALYSIS_CTOR(MC_HFJETS_MOD);
 
 
     /// Book histograms and initialise projections before the run
@@ -73,22 +73,65 @@ namespace Rivet {
       book(_h_ptFracB ,"ptfracB", 50, 0, 1.5);
       book(_h_eFracB ,"efracB", 50, 0, 1.5);
         // BLOCCO AGGIUNTO DA ME 
-      for (size_t d = 0; d < 5; ++d) {
-        book(_p_b_rho[d], d + 1, 1, 1);//iniziano con p e non con h perché
+  //    for (size_t d = 0; d < 5; ++d) {
+    //    book(_p_b_rho[d], d + 1, 1, 1);//iniziano con p e non con h perché
 	//sono profile1D, non Histogram1D
-        book(_p_Wjets_rho[d], d + 1, 2, 1);
-        book(_p_b_Psi[d], d + 1, 1, 2);
-        book(_p_Wjets_Psi[d], d + 1, 2, 2);
-    }
+      //  book(_p_Wjets_rho[d], d + 1, 2, 1);
+      //  book(_p_b_Psi[d], d + 1, 1, 2);
+       //book(_p_Wjets_Psi[d], d + 1, 2, 2);   }
+       //LO SOSTITUISCO CON QUEST'ALTRO BLOCCO COSI CHE TUTTI GLI 
+       //ISTOGRAMMI SIANO DICHIARATI ESPLICITAMENTE SENZA CHE IL FILE RICHIEDA
+       //INFORMAZIONI CHE IN REALTA STAREBBERO SUL FILE ATLAS2013...yoda.gz
+       //cosa che quindi manda in pappa il sistema quando runno
+       for (size_t d = 0; d < 5; ++d) {
+
+    book(_p_b_rho[d],
+         "b_rho_" + to_string(d),
+         10, 0., 0.4);
+
+    book(_p_Wjets_rho[d],
+         "Wjets_rho_" + to_string(d),
+         10, 0., 0.4);
+
+    book(_p_b_Psi[d],
+         "b_Psi_" + to_string(d),
+         10, 0., 0.4);
+
+    book(_p_Wjets_Psi[d],
+         "Wjets_Psi_" + to_string(d),
+         10, 0., 0.4);
+}     //fine blocco sostitivo
+
+      book(_h_bar_Wjets_width, "width_Wjets", 7, 0., 0.3);
+      book(_h_Wjets_ch_mult, "charged_mult_Wjets", 40, 0.5, 40.5);
+       book(_h_Wjets_pT, "pT_B_jet", 25, 7., 1025.);
     } //ok fin qui dovremmo essere a posto con le parentesi
 
+
+    //blocco necessario perché deltaR() così com'è richiede due rivet::fourmomentum
+// mentre constgenparticleptr è un hepmc3 qualcosa quindi deltaR non
+// capisce. Questo blocco serve per convertire hepmc3 e creare direttamente
+// un deltaR che funzioni a dovere.
+// Devo metterlo qui tra init() e analize() perché è la definzione di una funzione, e c++ non mi consente di definire una funzione dentro analize() (che a sua volta è un void {} quindi una funzione esso stesso mi sa)
+double deltaRJetGen(const Jet& jet,
+                   ConstGenParticlePtr p) const {
+
+    const FourMomentum pmom(
+        p->momentum().px(),
+        p->momentum().py(),
+        p->momentum().pz(),
+        p->momentum().e()
+    );
+
+    return deltaR(jet.momentum(), pmom);
+}
 
     /// Perform the per-event analysis
     void analyze(const Event& event) {
 
       // Get jets and heavy hadrons
     //  const Jets& jets = apply<JetFinder>(event, "Jets").jetsByPt();
-     const Jets& jets = apply<FastJets>(event, "JETS").jetsByPt(Cuts::pT > 7 * GeV);  //aggiunto il requisito sul pt minimo.
+     const Jets& jets = apply<FastJets>(event, "Jets").jetsByPt(Cuts::pT > 7 * GeV);  //aggiunto il requisito sul pt minimo.
 	    const Particles bhadrons = sortByPt(apply<HeavyHadrons>(event, "BCHadrons").bHadrons());
       const Particles chadrons = sortByPt(apply<HeavyHadrons>(event, "BCHadrons").cHadrons());
       MSG_DEBUG("# b hadrons = " << bhadrons.size() << ", # c hadrons = " << chadrons.size());
@@ -194,6 +237,18 @@ namespace Rivet {
         b_hadrons.push_back(p);
       }
 
+      // Select c-hadrons
+      /// @todo Use built-in identification on Particle, avoid HepMC
+      vector<ConstGenParticlePtr> c_hadrons;
+  //silenzio perché gia dichiarato a riga 210  //  vector<ConstGenParticlePtr> allParticles = HepMCUtils::particles(event.genEvent());
+      for (size_t i = 0; i < allParticles.size(); i++) {
+        ConstGenParticlePtr p = allParticles.at(i);
+        if (!(PID::isHadron(p->pdg_id()) && PID::hasCharm(p->pdg_id()))) continue;
+        if (p->momentum().perp() < 5 * GeV) continue;
+        c_hadrons.push_back(p);
+      }
+
+
       // Select b-jets as those containing a b-hadron
       /// @todo Use built-in dR < 0.3 Jet tagging, avoid HepMC
       vector<const Jet*> b_jets;
@@ -229,7 +284,7 @@ namespace Rivet {
               bool isCJet = false;
 
 for (ConstGenParticlePtr b : b_hadrons) {
-    if (deltaR(j.momentum(), b->momentum()) < 0.3) {
+    if (deltaRJetGen(*j, b) < 0.3) {
         isBJet = true;
         break;
     }
@@ -237,7 +292,7 @@ for (ConstGenParticlePtr b : b_hadrons) {
 
 if (!isBJet) {
     for (ConstGenParticlePtr c : c_hadrons) {
-        if (deltaR(j.momentum(), c->momentum()) < 0.3) {
+        if (deltaRJetGen(*j, c) < 0.3) {
             isCJet = true;
             break;
         }
@@ -245,11 +300,11 @@ if (!isBJet) {
 }
 
 if (isBJet)
-    b_jets.push_back(&j);
+    b_jets.push_back(j); //push_back vuole il puntatore al jet cioè j, non &j che sarebbe il puntatore di un puntatore
 else if (isCJet)
-    c_jets.push_back(&j);
+    c_jets.push_back(j);
 else
-    light_jets.push_back(&j);
+    light_jets.push_back(j);
     }
 
 //fin qui con le parentesi dovrebbe essere a posto
@@ -259,14 +314,15 @@ else
 const double nominalW   = 80.4 * GeV;
 const double nominalTop = 172.5 * GeV;
 
-const double sigmaW = 25. * GeV; //natural width di W e T
+const double sigmaW = 25. * GeV; //decay width and approximate experimental resolution 
 const double sigmaT = 35. * GeV;
 
 double bestChi2 = numeric_limits<double>::infinity();
 
 const Jet* bestJ1 = nullptr;
 const Jet* bestJ2 = nullptr;
-const Jet* bestB  = nullptr; 
+const Jet* bestB  = nullptr; //anche se questo in realtà non serve, ho già 
+//b_jets e non mi serve granché sapere qual è il b jet associato al W adronico 
 //in principio, non so a quale dei due b-jets è associato il W che
 // decade adronicamente (che è uno e non entrambi, dato che ho solo
 //  sample nonallhad/singlelep/dilep, quindi in effetti per questa
@@ -280,34 +336,39 @@ for (size_t a = 0; a < good_jets.size(); ++a) {
     // Do not use b jets as W candidates
     bool isB1 = false;
     for (ConstGenParticlePtr b : b_hadrons) {
-        if (deltaR(j1->momentum(), b->momentum()) < 0.3) {
-            isB1 = true;
-            break;
-        }
+      //blocco che serve per uniformare i tipi di j1->momentum e quello che sarebbe b->momentum(): il primo è un Rivet::FourMomentum, il secondo è un HepMC3FourVector e questa cosa manda in pappa rivet.
+     // const FourMomentum bhad_mom(
+   // b->momentum().px(),
+   // b->momentum().py(),
+   // b->momentum().pz(),
+  //  b->momentum().e() );
+
+if (deltaRJetGen(*j1, b) < 0.3) {
+    isB1 = true;
+    break;
+       }
+
     }
     if (isB1) continue;
 
 
     for (size_t b = a + 1; b < good_jets.size(); ++b)
-   //scrivere questo ciclo con b = a+1 impedisce che i jet j1 e j2 coincidano,
+   //scrivere questo ciclo con b = a+1 impedisce che i jet *j1 e j2 coincidano,
    //quindi di fatto va a ottimizzare il codice impedendo che j1=j2 e anche doppi conteggi
     {   const Jet* j2 = good_jets[b];
 
         // Do not use b jets as W candidates
         bool isB2 = false;
         for (ConstGenParticlePtr bhad : b_hadrons) {
-            if (deltaR(j2->momentum(), bhad->momentum()) < 0.3) {
+            if (deltaRJetGen(*j2, bhad) < 0.3) {
                 isB2 = true;
                 break;
             }
         }
         if (isB2) continue;
-
         // Reconstructed W
         const double mW =
             (j1->momentum() + j2->momentum()).mass();
-
-
         // Try both b jets
         for (const Jet* bjet : b_jets) {
 
@@ -330,9 +391,7 @@ for (size_t a = 0; a < good_jets.size(); ++a) {
 
 
             if (chi2 < bestChi2) {
-
-                bestChi2 = chi2;
-
+      		    bestChi2 = chi2;
                 bestJ1 = j1;
                 bestJ2 = j2;
                 bestB  = bjet;
@@ -345,27 +404,10 @@ for (size_t a = 0; a < good_jets.size(); ++a) {
       vector<const Jet*> W_jets;
       const bool hasGoodWJet = bestJ1 != NULL && bestJ2 != NULL && bestJ1 != bestJ2;
       if (hasGoodWJet) {
-        bool isOverlap1 = false, isOverlap2 = false;
-        for (const Jet* j : allJets) {
-          if (bestJ1 == j) continue;
-          const double dR1j = deltaR(bestJ1->momentum(), j->momentum());
-          if (dR1j < 0.8) {
-            isOverlap1 = true;
-            break;
-          }
-        }
-        for (const Jet* j : allJets) {
-          if (bestJ2 == j) continue;
-          const double dR2j = deltaR(bestJ2->momentum(), j->momentum());
-          if (dR2j < 0.8) {
-            isOverlap2 = true;
-            break;
-          }
-        }
-        if (!isOverlap1 && !isOverlap2) {
+  //qui in precedenza c'era tutto un blocco che richiedeva che i due bestJ fossero separati da DeltaR maggiore o uguale a 0.8 da un qualunque altro jet di alljet. Ma dato che nella selezione di alljet ci sono tutti i jet con pT superiore a 7 GeV, questa cosa è pericolosa perché rischia di ammazzarmi un sacco di Wjets. Meglio quindi togliere quella selezione e tenere solo la richiesta che i due Wjets non coincidano. 
           W_jets.push_back(bestJ1);
           W_jets.push_back(bestJ2);
-        }
+        
       }
       MSG_DEBUG(W_jets.size() << " W jets selected");
   //anche qui le parentesi potrebbero essere giuste
@@ -426,7 +468,10 @@ for (size_t a = 0; a < good_jets.size(); ++a) {
         vector<double> rings(10, 0);
         for (const Particle& p : WJet->particles()) {
           const double dR = deltaR(WJet->momentum(), p.momentum());
-          const size_t idR = (size_t)floor(dR / binWidth);
+          const size_t idR = (size_t)floor(dR / binWidth); //serve 
+	  //semplicemente a trasformare la distanza continua ΔR 
+	  //della particella dall'asse del jet nell'indice intero del bin 
+	  //radiale in cui cade la particella
           for (size_t i = idR; i < 10; ++i) rings[i] += p.pT();
         }
 
@@ -439,20 +484,43 @@ for (size_t a = 0; a < good_jets.size(); ++a) {
           _p_Wjets_rho[ipt]->fill(rcenter, rhoval);
           _p_Wjets_Psi[ipt]->fill(rcenter, psival);
         }
-      
+    
+      	if (W_jets.size()) 
+	for (const Jet* wjet : W_jets) {
+                 double W_num = 0., W_den = 0.;
+            long N_charged = 0;
+            for (const Particle& pp : wjet->particles()) {
+              if(pp.isStable()) {
+                W_num += pp.pT()*deltaR(wjet->momentum(),pp.momentum());
+                W_den += pp.pT();
+                if(pp.isCharged()) ++N_charged;}
+	      //if pp.isLepton non lo faccio perché nei light jets è parecchio improbabile che ci sia un leptone
+	     if (W_den)  _h_bar_Wjets_width->fill(W_num/W_den);
+            _h_Wjets_pT->fill(wjet->pT()/GeV);
+            if (N_charged) {
+              _h_Wjets_ch_mult->fill(N_charged);
+            
+            }
+            }
+  }  //
+
 	//FINE DEL BLOCCO AGGIUNTO DA ME 
       }
+}
     /// Normalise histograms etc., after the run
     void finalize() {
       normalize({_h_ptCJetLead, _h_ptCHadrLead, _h_ptBJetLead, _h_ptBHadrLead,
-            _h_ptFracC, _h_eFracC, _h_ptFracB, _h_eFracB, _h_p_Wjets_rho, _h_p_Wjets_Psi, _h_p_b_rho, _h_p_b_Psi});
-    }
+            _h_ptFracC, _h_eFracC, _h_ptFracB, _h_eFracB, _h_bar_Wjets_width,
+	    _h_Wjets_pT, _h_Wjets_ch_mult});
+      //, _p_Wjets_rho, _p_Wjets_Psi, _p_b_rho, _p_b_Psi}); //questi sono Profile1D, non vanno normalizzati a mano
+   }
 
 
     /// @name Histograms
     /// @{
     Histo1DPtr _h_ptCJetLead, _h_ptCHadrLead, _h_ptFracC, _h_eFracC;
     Histo1DPtr _h_ptBJetLead, _h_ptBHadrLead, _h_ptFracB, _h_eFracB;
+    Histo1DPtr   _h_bar_Wjets_width,_h_Wjets_pT, _h_Wjets_ch_mult;
     Profile1DPtr _p_b_rho[5];//inizio aggiunta mia
     Profile1DPtr _p_Wjets_rho[5];
     Profile1DPtr _p_b_Psi[5];
@@ -464,6 +532,6 @@ for (size_t a = 0; a < good_jets.size(); ++a) {
 
 	    
 
-  RIVET_DECLARE_PLUGIN(MC_HFJETS);
+  RIVET_DECLARE_PLUGIN(MC_HFJETS_MOD);
 
 }
